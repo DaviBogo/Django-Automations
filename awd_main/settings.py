0ff65bd9da90670fbf2f0533a8f6d1429b3803f8
Davi Vinicius Bogo <davi.bogo@dynamox.net>
2024-03-28 10:59:25 -0300

    Adiciona Celery e Redis

--- awd_main/settings.py
+++ awd_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_mca4k@gr9r=4xuefc4nw8!%6g&6#=t@(m5g63heceost^a4$u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'awd_main/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -134,3 +139,13 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
     50: "critical",
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS=True
+DEFAULT_FROM_EMAIL = 'Automate with Django <bogodavi10@gmail.com>'
+DEFAULT_TO_EMAIL = 'bogodavi10@gmail.com'
---
+++ dataentry/tasks.py
@@ -0,0 +1,32 @@
+from awd_main.celery import app
+from django.core.management import call_command
+from django.core.mail import EmailMessage
+from django.conf import settings
+from .utils import send_email_notification
+import time
+
+
+@app.task
+def celery_test_task():
+    time.sleep(5)
+
+    mail_subject = 'Test subject'
+    message = 'This is a test email'
+    to_email = settings.DEFAULT_TO_EMAIL
+
+    send_email_notification(mail_subject, message, to_email)
+
+    return 'Task executed successfully.'
+
+
+@app.task
+def import_data_task(file_path, model_name):
+    try:
+        call_command('importdata', file_path, model_name)
+    except Exception as e:
+        raise e
+    mail_subject = 'Import Data Completed'
+    message = 'Your data import has been succesful'
+    to_email = settings.DEFAULT_TO_EMAIL
+    send_email_notification(mail_subject, message, to_email)
+    return 'Data imported successfully.'
